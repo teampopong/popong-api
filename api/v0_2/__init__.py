b606588b591b1cb7020121d200f1f0d9ba3eb2c1
Lucy Park <me@lucypark.kr>
2014-10-25 08:16:20 +0900

    Add statements to API v0.2

--- api/v0_2/__init__.py
+++ api/v0_2/__init__.py
@@ -5,8 +5,10 @@ def init_app(app):
     from api.v0_2.bill import BillApi
     from api.v0_2.party import PartyApi
     from api.v0_2.person import PersonApi
+    from api.v0_2.statement import StatementApi
 
     register_api(app, BillApi, 'bill_api_v0_2', '/v0.2/bill/', pk_type='string')
     register_api(app, PartyApi, 'party_api_v0_2', '/v0.2/party/')
     register_api(app, PersonApi, 'person_api_v0_2', '/v0.2/person/')
+    register_api(app, StatementApi, 'statement_api_v0_2', '/v0.2/statement/')
 
---
+++ api/v0_2/statement.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+from api.view import ApiView
+from popong_models.statement import Statement
+
+
+class StatementApi(ApiView):
+    model = Statement
+    kind_single = 'statement'
+    kind_list = 'statements'
+
+    def to_dict(self, statement):
+        d = {
+            'id': statement.id,
+            'meeting_id': statement.meeting_id,
+            'person_id': statement.person_id,
+            'sequence': statement.sequence,
+            'speaker': statement.speaker,
+            'content': statement.content,
+            'date': statement.date,
+        }
+        return d
+
