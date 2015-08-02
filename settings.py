f87fe81bd7bb0038212bf26a2f55c7f99ae6e732
stray-leone <stray.leone@gmail.com>
2015-08-03 00:59:05 +0900

    update readme file for setting DB

---
+++ settings.py
@@ -0,0 +1,10 @@
+# Server
+DEBUG = False
+SCRIPT_NAME = '/'
+
+# Database
+SQLALCHEMY_URI = "postgresql://ID_HERE:PASSWD_HERE@HOST_HERE:5432/pokrdb"  # TODO: Fill
+#SQLALCHEMY_URI = "postgresql://postgres:POSTGRES_PASSWD_HERE@localhost:5432/pokrdb"  # EX
+
+# Documents
+API_DOCS_URL = "http://data.popong.com"
